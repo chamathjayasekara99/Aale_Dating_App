c14fbbd7c34b3f34fd45c043516abc44108b5108
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-03-18 18:41:34 +0530

    Added new styles to drawable folder and  those are button_styles.xml and custom_inputs.xml login activity added

---
+++ app/src/main/java/com/example/aale_dating_app/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.aale_dating_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//     //   this line hides the action bar
+//        getSupportActionBar().hide();
+//        //I have added omthing
+    }
+}
