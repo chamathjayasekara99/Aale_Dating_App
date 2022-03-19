f96415b9f96c50c14dd9aa542f5a7df647491a0a
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-03-19 08:31:51 +0530

    Added a table

--- app/src/main/java/com/example/aale_dating_app/AdminUserManageActivity.java
+++ app/src/main/java/com/example/aale_dating_app/AdminUserManageActivity.java
@@ -3,15 +3,12 @@ package com.example.aale_dating_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
-public class LoginActivity extends AppCompatActivity {
+public class AdminUserManageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_adminusermanager);
     }
 }
